31e5730248aea02ffc7814177b3886590ca668eb
BENAZIZA <benazizahassen01@gmail.com>
2020-06-22 00:27:39 +0100

    correction checkpoint

--- src/UserList.js
+++ src/UserList.js
@@ -6,20 +6,39 @@ import './App.css';
 
 const UserList =()=>
 {
-	const [users,setUsers] = useState([]);
+	const [users,setUsers] = useState({list : []});
 	useEffect(()=>
-	{const fetchData = async ()=>
-	{
-	const result= await axios.get('https://jsonplaceholder.typicode.com/users');
-	setUsers(result.data);
-    } ;
-   fetchData();},[]);
+		{const fetchData = async ()=>
+			{
+				const result= await axios.get('https://jsonplaceholder.typicode.com/users');
+				setUsers({list:result.data});
+			} ;
+			fetchData();},[]);
 	return(
 		<div className='styleUsersList'>
-         <h1>User's name</h1>
-		<ul className="list-group">
-		{users.map((user)=> <li className="list-group-item list-group-item-dark" key={user.id} > {user.name}</li> )}
-		</ul>
+		<h1>User's name</h1>
+		<table className="table table-striped">
+		<thead>
+		<tr>
+		<th scope="col">id</th>
+		<th scope="col">name</th>
+		<th scope="col">username</th>
+		<th scope="col">Email</th>
+		<th scope="col">Adress</th>
+		</tr>
+		</thead>
+		 <tbody>	
+		{users.list.map((user)=> 
+			<tr key={user.id}>
+			<th scope="row" > {user.id}</th>
+			<td  > {user.name}</td>
+			<td  > {user.username}</td>
+			<td  > {user.email}</td>
+			<td  > {user.address.street}</td>
+			</tr> 
+			)}	
+		</tbody>	
+		</table>
 		</div>)
-}
-export default UserList;
+		}
+		export default UserList;
